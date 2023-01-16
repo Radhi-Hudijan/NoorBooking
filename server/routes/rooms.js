d68d14a477d5b74028339f635f142aafd65f50cc
Radhi Hudijan <radhif2009@gmail.com>
2023-01-16 09:13:01 +0100

    room controllers has been created

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,5 +1,28 @@
 const express = require("express");
+const { verifyAdmin } = require("../utils/verifyToken.js");
+const {
+  createRoom,
+  updateRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+} = require("../controllers/room.js");
 
 const router = express.Router();
+
+//create POST
+router.post("/:hotelId", verifyAdmin, createRoom);
+
+//update PUT
+router.put("/:id", verifyAdmin, updateRoom);
+
+//delete
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom);
+
+//get
+router.get("/:id", getRoom);
+
+//get all
+router.get("/", getRooms);
 
 module.exports = router;
